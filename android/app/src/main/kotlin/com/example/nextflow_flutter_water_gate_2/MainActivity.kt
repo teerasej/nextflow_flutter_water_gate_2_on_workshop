59df139594e77b6bf1831d9d8f4bedbad5487a4a
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-11-02 15:33:31 +0700

    แบบฟอร์มหน้าคำนวนพร้อมแล้ว

---
+++ android/app/src/main/kotlin/com/example/nextflow_flutter_water_gate_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nextflow_flutter_water_gate_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:nextflow_flutter_water_gate_2/pages/gate_calculator_page/gate_calculator_page.dart';
+import 'package:nextflow_flutter_water_gate_2/pages/home_page/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Nextflow Water Gate',
+      theme: ThemeData(
+        primaryColor: Colors.blue[900],
+      ),
+      initialRoute: '/gate-calculator',
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => HomePage(),
+        ),
+        GetPage(
+          name: '/gate-calculator',
+          page: () => GateCalculatorPage(),
+        ),
+      ],
+    );
+  }
+}
